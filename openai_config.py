8348d566311ce58ea3dcd1e0dcdc04bb7c71ceb7
mogesTesema <moges2421@gmail.com>
2025-12-27 21:21:08 +0300

    ask_gpt4 function to ask LLM model

--- openai_config.py
+++ openai_config.py
@@ -1,21 +1,27 @@
 from openai import OpenAI
 from dotenv import load_dotenv
+from IPython.display import Markdown,display
+
 import os
 load_dotenv()
 GITHUB_OPENAI_API_KEY = os.getenv("GITHUB_OPENAI_API_KEY")
+OPENAI_URL = os.getenv("OPENAI_URL","https://models.inference.ai.azure.com")
 
 client = OpenAI(
-    base_url="https://models.inference.ai.azure.com",
+    base_url=OPENAI_URL,
     api_key=GITHUB_OPENAI_API_KEY
 )
 
-response = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {"role": "system", "content": "You are a concise technical assistant."},
-        {"role": "user", "content": "how to learn software engineering and AI engineering faster than ever using ai tools"}
-    ],
-    temperature=0.2
-)
+def ask_gpt4o(user_prompt,system_prompt="you are good assistant"):
 
-print(response.choices[0].message.content)
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt}
+        ],
+        temperature=0.2
+    )
+
+    return response.choices[0].message.content
+print(ask_gpt4o("how to send async request to any backend in python without any framework? give me some example code"))
