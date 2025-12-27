f8f69388d85baf97a896ee7ba29de44c31ed85fc
mogesTesema <moges2421@gmail.com>
2025-12-27 22:22:31 +0300

    top_p,presence_penalty,frequency_penalty parameter adjusting

--- openai_config.py
+++ openai_config.py
@@ -20,7 +20,10 @@ def ask_gpt4o(user_prompt,system_prompt="you are good assistant"):
             {"role": "system", "content": system_prompt},
             {"role": "user", "content": user_prompt}
         ],
-        temperature=0.2
+        temperature=0.2,
+        top_p=0.3,
+        presence_penalty=0,
+        frequency_penalty=0
     )
 
     return response.choices[0].message.content
